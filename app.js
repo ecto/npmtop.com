8ddd19a918ef4533965944b9f275fbf3a6ae4181
Cam Pedersen <diffference@gmail.com>
2012-01-11 22:40:33 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var authors;
+var limit = 20;
+var skip = 0;
+
+$(document).ready(function () {
+  $.get('http://search.npmjs.org/_view/author?group=true',
+    function (data) {
+      try {
+        data = JSON.parse(data);
+      } catch (e) {
+        alert('YOU\'VE GOT TO BE KIDDING ME. UPGRADE YOUR BROWSER');
+      }
+      authors = data.rows.sort(function (a, b) {
+        return b.value - a.value; // value descending
+      });
+      $('#content').text('');
+      renderTop();
+      $(document).scroll(function () {
+        if ($(this).scrollTop() + $(window).height() >= $(this).height() - 50) {
+          renderTop();
+        }
+      });
+    }
+  );
+});
+
+function renderTop () {
+  $('#content').append('<pre>rank     packages   author\n\
+----------------------------------------------</pre>');
+  for (var i = skip; i < limit + skip; i++) {
+    $('#content').append(
+      '<pre><strong>#' + (i + 1) + '</strong>' + '       ' +
+      authors[i].value + '        ' + authors[i].key + '</pre>'
+    );
+  }   
+  skip += limit;
+}
